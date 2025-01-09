12a80e837350f2e0f8800b2b2b70e48f2a3925e5
hoang <letterc67@localhost.localdomain>
2025-01-09 21:52:30 +0700

    add forgot password api

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,13 +1,18 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"onichan/database"
 	"onichan/model"
+	"onichan/services"
 	"onichan/utils"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -32,9 +37,19 @@ type ChangeEmailRequest struct {
 	Password string `json:"password" binding:"required"`
 }
 
+type ForgotPasswordRequest struct {
+	Email string `json:"email" binding:"required"`
+}
+
+type ResetForgottenPasswordRequest struct {
+	Token string `json:"token" binding:"required"`
+}
+
 type ChangeAvatarRequest struct {
 	AvatarURL string `json:"avatar_url" binding:"required"`
 }
+
+var EMAIL_EXPIRATION, _ = strconv.Atoi(os.Getenv("EMAIL_EXPIRATION"))
 
 // Register godoc
 // @Summary      Register a new user
@@ -268,4 +283,108 @@ func ChangeAvatar(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Avatar updated successfully"})
+}
+
+// ForgotPassword godoc
+// @Summary      Initiate password reset
+// @Description  Generates a token and sends a password-reset email to the user
+// @Tags         Auth
+// @Accept       json
+// @Produce      json
+// @Param        ForgotPassword body ForgotPasswordRequest true "Forgot Password Payload"
+// @Success      200 {object} gin.H{"message": "Email sent"}
+// @Failure      400 {object} gin.H{"error": "Bad request"}
+// @Failure      404 {object} gin.H{"error": "User not found"}
+// @Failure      500 {object} gin.H{"error": "Internal server error"}
+// @Router       /auth/forgot-password [post]
+func ForgotPassword(c *gin.Context) {
+	var payload ForgotPasswordRequest
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user model.User
+	if err := database.Database.First(&user, "email = ?", payload.Email).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	token, err := utils.GenerateJWTForgotPassword(user.ID, user.PasswordHash)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	err = services.SendEmail(user.Email, "Change Password", "Click the link to change your password: "+os.Getenv("FRONTEND_URL")+"/reset-password?token="+token)
+
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send email"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Email sent"})
+}
+
+// ResetForgottenPassword godoc
+// @Summary      Reset a forgotten password
+// @Description  Validates the token, generates a new password, updates the password in the database, and returns the new password
+// @Tags         Auth
+// @Accept       json
+// @Produce      json
+// @Param        ResetForgottenPassword body ResetForgottenPasswordRequest true "Reset Forgotten Password Payload"
+// @Success      200 {object} gin.H{"password": "newly-generated-password"}
+// @Failure      400 {object} gin.H{"error": "Invalid token or bad request"}
+// @Failure      404 {object} gin.H{"error": "User not found"}
+// @Failure      500 {object} gin.H{"error": "Failed to update password"}
+// @Router       /auth/reset-password [post]
+func ResetForgottenPassword(c *gin.Context) {
+	var payload ResetForgottenPasswordRequest
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := utils.ValidateJWT(payload.Token)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid token"})
+		return
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	userID := uint(claims["user_id"].(float64))
+	hash := claims["hash"].(string)
+
+	fmt.Println(userID, hash)
+
+	var user model.User
+	if err := database.Database.First(&user, userID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	if hash != user.PasswordHash {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid token"})
+		return
+	}
+
+	password := utils.GetRandomAlphaString(8)
+	salt := utils.GetToken(32)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(salt+password), bcrypt.DefaultCost)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not hash password"})
+		return
+	}
+
+	if err := database.Database.Model(&user).Updates(map[string]interface{}{
+		"password_hash": string(hashedPassword),
+		"salt":          salt,
+	}).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update password"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"password": password})
 }
--- go.mod
+++ go.mod
@@ -54,6 +54,8 @@ require (
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/tools v0.28.0 // indirect
 	google.golang.org/protobuf v1.36.1 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.11 // indirect
